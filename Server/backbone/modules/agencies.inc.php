3641938cd8f10d97cdbebe1f5cb0528e07769ad6
RoyalBis <Royal.Bissell@gmail.com>
2015-11-09 17:50:01 -0700

    Royal Syncing
    ...

--- Server/backbone/modules/agencies.inc.php
+++ Server/backbone/modules/agencies.inc.php
@@ -5,9 +5,8 @@
     $query = "SELECT * FROM `agencies`";
     $agencies = $dB->get($query);
 
-
-
     print_r($agencies);
+
 
     //var_dump($customers[0]->CustomerId);
     //echo $customers[0]->CustomerId;
