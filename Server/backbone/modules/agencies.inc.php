9a2bd7751d2c7b5238fe6e573a83cff6cc3515e2
RoyalBis <Royal.Bissell@gmail.com>
2015-11-10 10:58:16 -0700

    Agencies.inc.php
    ...

--- Server/backbone/modules/agencies.inc.php
+++ Server/backbone/modules/agencies.inc.php
@@ -1,13 +1,35 @@
 <?php
     include("../global.php");
-    $dB = new DB('travelexperts');
 
-    $query = "SELECT * FROM `agencies`";
-    $agencies = $dB->get($query);
+///////////////////////////////////////////////////////////////////////////////
 
-    print_r($agencies);
+    class Agencies {
 
+        public $myAgencies;
+        public  $myHtmlString;
 
+        function __constructor () {
+            $this->getAgencies();
+        }
+
+        function getAgencies () {
+            $dB = new DB('travelexperts');
+            $query = "SELECT * FROM `agencies`";
+            $this->myAgencies = $dB->get($query);
+        }
+
+        function formatAgencies () {
+            // foreach ($this->myAgencies as $agency) {
+                print_r ($this->myAgencies);
+                // print_r ($agency . "<br /><br />");
+                // $this->myHtmlString = $this->$myHtmlOpen . $this->$myContent . $this->$myHtmlClose;
+            // }
+        }
+    }
+
+    $agencies = new Agencies ();
+    print_r ($agencies->getAgencies ());
+    print_r ($agencies);
     //var_dump($customers[0]->CustomerId);
     //echo $customers[0]->CustomerId;
 
