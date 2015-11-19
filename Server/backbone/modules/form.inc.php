db6ba68c6342ebaf14ed746e4978d68a3e2101e1
heidicantalejo <hcantalejo@gmail.com>
2015-11-18 18:26:24 -0700

    Edited Comments

--- Server/backbone/modules/form.inc.php
+++ Server/backbone/modules/form.inc.php
@@ -5,9 +5,7 @@
   Date:        2015-11-18 
   Description: This file contains the class to handle form submissions.
                Specific form classes will extend from this class.
-  *****************************************************************************/ 
-
-  /*****************************************************************************
+--------------------------------------------------------------------------------
   Generic class with properties and methods to handle all form submits
   *****************************************************************************/
   class FormHandle {
